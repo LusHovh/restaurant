6d825b9f233467fa148c805a33773a4029291e32
LusHovh <lusluslusin@gmail.com>
2017-08-19 15:42:34 +0400

    almost done Home page

--- src/less/index.js
+++ src/less/index.js
@@ -1,1 +1,17 @@
 
+    $(document).scroll(function() {
+        if ($(document).scrollTop() > 600) {
+            $('.navbar').css({'background':'linear-gradient(to right, rgba(255,0,0,0), rgba(0,0,0,1))', 'boxShadow' :' 0px 0px 5px rgb(0, 0, 0)'});
+            $('ul.nav').css({'marginTop':'5px'});
+        } else {
+            $('.navbar').css({'background':'transparent',  'boxShadow' :' unset'});
+            $('ul.nav').css({'marginTop':'5px'});
+        }
+        // console.log($(document).scrollTop());
+    });
+    $(function() {
+        $.scrollify({
+            section : 'section'
+        });
+    });
+
