682fccfe292784946d491809a9e6b19a145d8044
LusHovh <lusluslusin@gmail.com>
2017-08-20 22:10:47 +0400

    Home Page done

--- src/less/index.js
+++ src/less/index.js
@@ -1,17 +1,43 @@
-
-    $(document).scroll(function() {
-        if ($(document).scrollTop() > 600) {
-            $('.navbar').css({'background':'linear-gradient(to right, rgba(255,0,0,0), rgba(0,0,0,1))', 'boxShadow' :' 0px 0px 5px rgb(0, 0, 0)'});
-            $('ul.nav').css({'marginTop':'5px'});
-        } else {
-            $('.navbar').css({'background':'transparent',  'boxShadow' :' unset'});
-            $('ul.nav').css({'marginTop':'5px'});
-        }
-        // console.log($(document).scrollTop());
+// Scroll to a div #id, & Scrollify plugin
+$(document).ready(function() {
+    $('#my_link_about').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#about').offset().top
+        }, 1000);
     });
-    $(function() {
-        $.scrollify({
-            section : 'section'
-        });
-    });
+});
 
+$(document).ready(function() {
+    $('#my_link_home').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#top').offset().top
+        }, 1000);
+    });
+});
+
+$(document).scroll(function() {
+    if ($(document).scrollTop() > 600) {
+        $('.navbar').css({'background':'linear-gradient(to right, rgba(255,0,0,0), rgba(0,0,0,1))'});
+        $('ul.nav').css({'marginTop':'5px'});
+    } else {
+        $('.navbar').css({'background':'transparent',  'boxShadow' :' unset'});
+        $('ul.nav').css({'marginTop':'5px'});
+    }
+});
+
+$(function() {
+    $.scrollify({
+        section : 'section'
+    });
+});
+// Jquery-UI Datepicker plugin
+
+$( function() {
+    $( '#datepicker' ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange: '-100:+0'
+    });
+} );
